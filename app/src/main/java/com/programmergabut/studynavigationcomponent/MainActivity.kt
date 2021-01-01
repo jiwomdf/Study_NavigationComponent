90b933077604eb215d640f1ee740ff043c3629e0
jiwomdf <mdf.jiwo@outlook.com>
2021-01-01 14:18:53 +0700

    finish tutorial

--- app/src/main/java/com/programmergabut/studynavigationcomponent/MainActivity.kt
+++ app/src/main/java/com/programmergabut/studynavigationcomponent/MainActivity.kt
@@ -7,13 +7,13 @@ import android.view.MenuItem
 import androidx.navigation.NavController
 import androidx.navigation.NavGraph
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.onNavDestinationSelected
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,8 +22,16 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
 
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.homeFragment, R.id.searchFragment),
+            drawer_layout
+        )
+
         setSupportActionBar(toolbar)
-        setupActionBarWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        bottom_nav.setupWithNavController(navController)
+        nav_view.setupWithNavController(navController)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -42,6 +50,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp() || super.onSupportNavigateUp()
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
---
+++ app/src/main/java/com/programmergabut/studynavigationcomponent/SearchFragment.kt
@@ -0,0 +1,6 @@
+package com.programmergabut.studynavigationcomponent
+
+import androidx.fragment.app.Fragment
+
+class SearchFragment: Fragment(R.layout.fragment_search) {
+}
